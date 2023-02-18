9477dac98b162f41712fba8dbb801ceb0a31e289
ataur28 <ataurrahman862828@gmail.com>
2023-02-18 15:46:10 +0600

    added error handiling part

--- js/calculate.js
+++ js/calculate.js
@@ -3,6 +3,15 @@ document.getElementById('triangle-calcutale-btn').addEventListener('click', func
     const triangleBValue = inputFieldValue('triangle-b-input');
     const triangleHValue = inputFieldValue('triangle-h-input');
 
+    if(triangleBValue<=0 || isNaN(triangleBValue)){
+        alert('Please Enter Correct Value for b field');
+        return;
+    }
+    else if(triangleHValue<=0 || isNaN(triangleHValue)){
+        alert('Please Enter Correct Value for h field');
+        return;
+    }
+
     const area = 0.5 * triangleBValue * triangleHValue;
 
     const triangleText = textFieldValue('triangle-text');
@@ -36,6 +45,15 @@ document.getElementById('rectangle-calculate-btn').addEventListener('click', fun
     const rectangleWValue = inputFieldValue('rectangle-w-input');
     const rectanglelValue = inputFieldValue('rectangle-l-input');
 
+    if(rectangleWValue<=0 || isNaN(rectangleWValue)){
+        alert('Please Enter Correct Value for w field');
+        return;
+    }
+    else if(rectanglelValue <=0 || isNaN(rectanglelValue)){
+        alert('Please Enter Correct Value for l field');
+        return;
+    }
+
     const area = rectangleWValue * rectanglelValue;
 
     const rectangleText = textFieldValue('rectangle-text');
@@ -48,6 +66,15 @@ document.getElementById('rectangle-calculate-btn').addEventListener('click', fun
 document.getElementById('parallelogram-calculate-btn').addEventListener('click', function () {
     const parallelogramBValue = inputFieldValue('parallelogram-b-input');
     const parallelogramHValue = inputFieldValue('parallelogram-h-input');
+
+    if(parallelogramBValue<=0 || isNaN(parallelogramBValue)){
+        alert('Please Enter Correct Value for b field');
+        return;
+    }
+    else if(parallelogramHValue <=0 || isNaN(parallelogramHValue)){
+        alert('Please Enter Correct Value for h field');
+        return;
+    }
 
     const area = parallelogramBValue * parallelogramHValue;
 
@@ -63,6 +90,15 @@ document.getElementById('rhombus-calculate-btn').addEventListener('click', funct
     const rhombusD1Value = inputFieldValue('rhombus-d1-input');
     const rhombusD2Value = inputFieldValue('rhombus-d2-input');
 
+    if(rhombusD1Value<=0 || isNaN(rhombusD1Value)){
+        alert('Please Enter Correct Value for d1 field');
+        return;
+    }
+    else if(rhombusD2Value <=0 || isNaN(rhombusD2Value)){
+        alert('Please Enter Correct Value for d2 field');
+        return;
+    }
+
     const area = 0.5* rhombusD1Value * rhombusD2Value ;
 
     const rhombusText = textFieldValue('rhombus-text');
@@ -76,6 +112,15 @@ document.getElementById('pentagon-calculate-btn').addEventListener('click', func
     const pentagonPValue = inputFieldValue('pentagon-p-input');
     const pentagonBValue = inputFieldValue('pentagon-b-input');
 
+    if(pentagonPValue<=0 || isNaN(pentagonPValue)){
+        alert('Please Enter Correct Value for p field');
+        return;
+    }
+    else if(pentagonBValue<=0 || isNaN(pentagonBValue)){
+        alert('Please Enter Correct Value for b field');
+        return;
+    }
+
     const area = 0.5* pentagonPValue * pentagonBValue ;
 
     const pentagonText = textFieldValue('pentagon-text');
@@ -88,6 +133,15 @@ document.getElementById('pentagon-calculate-btn').addEventListener('click', func
 document.getElementById('ellipse-calculate-btn').addEventListener('click', function () {
     const ellipseAValue = inputFieldValue('ellipse-a-input');
     const ellipseBValue = inputFieldValue('ellipse-b-input');
+
+    if(ellipseAValue<=0 || isNaN(ellipseAValue)){
+        alert('Please Enter Correct Value for a field');
+        return;
+    }
+    else if(ellipseBValue<=0 || isNaN(ellipseBValue)){
+        alert('Please Enter Correct Value for b field');
+        return;
+    }
 
     const area = 3.14 * ellipseAValue * ellipseBValue ;
 
