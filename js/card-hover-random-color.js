aa10fcce55625b090c0dab51d2c36a49aed1e71f
ataur28 <ataurrahman862828@gmail.com>
2023-02-18 15:32:09 +0600

    added all card random hover part

--- js/card-hover-random-color.js
+++ js/card-hover-random-color.js
@@ -6,22 +6,66 @@ function randomColor() {
     return 'rgb(' + color.join(', ') + ')';
 }
 //Triangle
-document.getElementById('card-hover-random-color').addEventListener('mouseover', event => {
-    const hoverColor = document.getElementById('card-hover-random-color');
+document.getElementById('card-one-hover-random-color').addEventListener('mouseover', event => {
+    const hoverColor = document.getElementById('card-one-hover-random-color');
     hoverColor.style.backgroundColor = randomColor();
 });
 
-document.getElementById('card-hover-random-color').addEventListener('mouseout', event => {
-    const hoverColor = document.getElementById('card-hover-random-color');
-    hoverColor.style.backgroundColor = '';
+document.getElementById('card-one-hover-random-color').addEventListener('mouseout', event => {
+    const hoverColor = document.getElementById('card-one-hover-random-color');
+    hoverColor.style.backgroundColor ='';
 });
 
 // Rectangle
+document.getElementById('card-two-hover-random-color').addEventListener('mouseover', event => {
+    const hoverColor = document.getElementById('card-two-hover-random-color');
+    hoverColor.style.backgroundColor = randomColor();
+});
+
+document.getElementById('card-two-hover-random-color').addEventListener('mouseout', event => {
+    const hoverColor = document.getElementById('card-two-hover-random-color');
+    hoverColor.style.backgroundColor = '';
+});
 
 // Parallelogram
+document.getElementById('card-three-hover-random-color').addEventListener('mouseover', event => {
+    const hoverColor = document.getElementById('card-three-hover-random-color');
+    hoverColor.style.backgroundColor = randomColor();
+});
+
+document.getElementById('card-three-hover-random-color').addEventListener('mouseout', event => {
+    const hoverColor = document.getElementById('card-three-hover-random-color');
+    hoverColor.style.backgroundColor = '';
+});
 
 // Rhombus
+document.getElementById('card-four-hover-random-color').addEventListener('mouseover', event => {
+    const hoverColor = document.getElementById('card-four-hover-random-color');
+    hoverColor.style.backgroundColor = randomColor();
+});
+
+document.getElementById('card-four-hover-random-color').addEventListener('mouseout', event => {
+    const hoverColor = document.getElementById('card-four-hover-random-color');
+    hoverColor.style.backgroundColor = '';
+});
 
 // Pentagon
+document.getElementById('card-five-hover-random-color').addEventListener('mouseover', event => {
+    const hoverColor = document.getElementById('card-five-hover-random-color');
+    hoverColor.style.backgroundColor = randomColor();
+});
 
+document.getElementById('card-five-hover-random-color').addEventListener('mouseout', event => {
+    const hoverColor = document.getElementById('card-five-hover-random-color');
+    hoverColor.style.backgroundColor = '';
+});
 // Ellipse
+document.getElementById('card-six-hover-random-color').addEventListener('mouseover', event => {
+    const hoverColor = document.getElementById('card-six-hover-random-color');
+    hoverColor.style.backgroundColor = randomColor();
+});
+
+document.getElementById('card-six-hover-random-color').addEventListener('mouseout', event => {
+    const hoverColor = document.getElementById('card-six-hover-random-color');
+    hoverColor.style.backgroundColor = '';
+});
